0a34ad67979192d7b3e2f3dd573999ccdd6502a2
Ganesh Devkar <ganesh.devkar@quodeck.com>
2022-12-12 12:03:21 +0530

    Add Home Page

---
+++ src/app/home/Home.react.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Image } from "semantic-ui-react";
+import { Typography, Grid } from "@mui/material";
+import Img from "../../assets/img-promo.png";
+import logo from "../../assets/logo1.png";
+import "semantic-ui-css/semantic.min.css";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <Grid
+      container
+      spacing={2}
+      style={{ minHeight: "100vh", backgroundColor: "#454545" }}
+      sx={{ pl: 8, pr: 8, pt: 2, overflow: "scroll" }}
+      className="main-container"
+    >
+      <Grid item xs={12} md={12} sm={12} className="center-align">
+        <Image src={logo} alt="" height="50m" width="180em" />
+      </Grid>
+      <Grid item xs={12} md={6} sm={6} className="center-align">
+        <Typography
+          sx={{
+            color: "#FFFFFF",
+            fontSize: "4em",
+            lineHeight: "1em",
+            transform: "scale(1, 1.25)",
+            fontWeight: "300",
+          }}
+        >
+          <b>Cost Estimator</b>
+        </Typography>
+        <Typography
+          sx={{
+            fontFamily: "normal",
+            color: "#fff",
+            fontSize: "1.5em",
+            margin: "1em 0em",
+            lineHeight: "1em",
+          }}
+        >
+          <b>Kick Start your journy to awesome content with quodeck</b>
+        </Typography>
+
+        <button className="home-started-btn"> GET STARTED</button>
+      </Grid>
+      <Grid item xs={12} md={6} sm={6}>
+        <Image src={Img} alt="" height="auto" />
+      </Grid>
+    </Grid>
+  );
+}
