7bc52cf1d91c99607fd11e6181a1b63c653e5ab2
Ganesh Devkar <ganesh.devkar@quodeck.com>
2022-12-13 15:05:50 +0530

    make responsive home page

--- src/app/home/Home.react.jsx
+++ src/app/home/Home.react.jsx
@@ -1,64 +1,97 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { Image } from "semantic-ui-react";
-import { Typography, Grid } from "@mui/material";
+import { Button, Typography, Grid } from "@mui/material";
 import Img from "../../assets/img-promo.png";
 import logo from "../../assets/logo1.png";
 import "semantic-ui-css/semantic.min.css";
 import QuestionSlider from "../QuestionSlider/QuestionSlider.react";
-import RecommendationSection from "../recommendationSection/RecommendationSection.react"
+import RecommendationSection from "../recommendationSection/RecommendationSection.react";
 import "./Home.css";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
   const [rOpen, setROpen] = useState(false);
   let handleModal = () => {
-    setOpen(false)
-    setROpen(true)
-  }
+    setOpen(false);
+    setROpen(true);
+  };
 
   return (
     <Grid
       container
       spacing={2}
       style={{ minHeight: "100vh", backgroundColor: "#454545" }}
-      sx={{ pl: 8, pr: 8, pt: 2, overflow: "scroll" }}
+      sx={{ pl: 8, pr: 8 }}
       className="main-container"
     >
       <QuestionSlider open={open} onSubmit={() => handleModal()} />
-      <RecommendationSection open={rOpen} onClose={(e) => setROpen(e)} level={4} showButton/>
-      <Grid item xs={12} md={12} sm={12} className="center-align">
+      <RecommendationSection
+        open={rOpen}
+        onClose={(e) => setROpen(e)}
+        level={4}
+        showButton
+      />
+      <div className="logoimg">
         <Image src={logo} alt="" height="50m" width="180em" />
-      </Grid>
-      <Grid item xs={12} md={6} sm={6} className="center-align">
+      </div>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sm={6}
+        className="center-align"
+        order={{ xs: 2, sm: 2, lg: 1 }}
+      >
         <Typography
           sx={{
-            color: "#FFFFFF",
-            fontSize: "4em",
-            lineHeight: "1em",
-            transform: "scale(1, 1.25)",
-            fontWeight: "300",
-          }}
-        >
-          <b>Cost Estimator</b>
-        </Typography>
-        <Typography
-          sx={{
-            fontFamily: "normal",
+            padding: "0 0 15px 0",
             color: "#fff",
-            fontSize: "1.5em",
-            margin: "1em 0em",
-            lineHeight: "1em",
+            fontSize: "48px",
+            lineHeight: "54px",
+            fontWeight: 700,
           }}
         >
-          <b>Click Start your journy to awesome content with quodeck</b>
+          <h1>Cost Estimator</h1>
         </Typography>
-
-        <button className="home-started-btn" onClick={() => setOpen(true)}>
-          GET STARTED
-        </button>
+        <Typography
+          sx={{
+            padding: "0 0 15px 0",
+            fontSize: "18px",
+            lineHeight: "25px",
+            color: "#fff",
+            letterSpacing: "0.5px",
+            fontWeight: 100,
+          }}
+        >
+          Kick Start your journy to awesome content with quodeck
+        </Typography>
+        <Button
+          variant="outlined"
+          sx={{
+            background: "transparent",
+            border: "1px solid #fff",
+            borderRadius: "4px",
+            display: "inline-block",
+            padding: " 11px 32px",
+            fontSize: " 15px",
+            lineHeight: "28px",
+            fontWeight: 500,
+            textTransform: "uppercase",
+            transition: "1s",
+            marginTop: "2em",
+            color: "#fff",
+            ":hover": {
+              backgroundColor: "#ffbf00",
+              color: "#000",
+            },
+          }}
+          onClick={() => setOpen(true)}
+        >
+          Get Started
+        </Button>
       </Grid>
-      <Grid item xs={12} md={6} sm={6}>
+      <Grid item xs={12} md={6} sm={6} order={{ xs: 1, sm: 2, lg: 2 }}>
         <Image src={Img} alt="" height="auto" />
       </Grid>
     </Grid>
